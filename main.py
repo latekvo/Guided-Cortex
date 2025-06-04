7acbd51dbdd4e8f4b927892a285fd52e0493eabd
LatekVon <latkaignacy@gmail.com>
2025-06-04 02:41:22 +0200

    fix crashes on command exec

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from models.agents.base import Agent
-from models.agents.manager import Manager
-from runtimes.runtime import is_linux_ok
+from langchain_core.messages import HumanMessage
 
+from debug.visualizer import visualize_tree
+from models.agents.manager import Manager
+from models.chats import ExternalChat
+from runtimes.runtime import is_linux_ok
 
 # note: We're not doing any persistent thinking functions
 #       Managers should be able to divide the tasks and respond to events,
@@ -12,25 +14,24 @@ from runtimes.runtime import is_linux_ok
 # note: there doesn't seem to be a need for an id-based pool of agents, thus sticking to a ref-tree
 
 
-def visualize_tree(root: Agent, indent=0):
-    print(" " * indent + f"- [{root.type}] {root.label}")
-    if isinstance(root, Manager):
-        for child in root.children:
-            visualize_tree(child, indent + 1)
-
-
 # todo: impl async user chat or injection CLI
 #       user should be able to contact ANY member of the hierarchy
+
+USER_ID = "ROOT_USER"
+
+# todo: add mock UserAgent for injecting user into the tree
 
 
 def main():
     is_linux_ok()
+    root_manager = Manager(USER_ID, "Execute the user's orders", "Chief Director")
+    root_chat = ExternalChat(USER_ID, root_manager.id, "Communication with the user.")
+    root_manager.external_chats[USER_ID] = root_chat
 
-    root_manager = Manager(
-        "ROOT_USER",
-        'Say "Hello World", then create 2 workers each also saying "Hello world"',
-        "Chief Director",
+    root_chat.chat_history.append(
+        HumanMessage('Create 2 workers each saying "Hello world"')
     )
+
     root_manager.run_turn_recurse()
     visualize_tree(root_manager)
 
--- models/chats.py
+++ models/chats.py
@@ -2,16 +2,10 @@ from langchain_core.messages import BaseMessage
 
 
 class ExternalChat:
-    initiator_id: str
-    target_id: str
-    opening_reason: str
-    chat_summary: str
-    chat_history: list[BaseMessage]
-
+    # Note: ExternalChat requires manual dealloc
     def __init__(self, init_id: str, target_id: str, opening_reason: str):
-        self.initiator_id = init_id
-        self.target_id = target_id
-        self.opening_reason = opening_reason
-        # generate after every N turns, make it broad
-        self.chat_summary = "Summary not available yet."
-        self.chat_history = []
+        self.initiator_id: str = init_id
+        self.target_id: str = target_id
+        self.opening_reason: str = opening_reason
+        self.chat_history: list[BaseMessage] = []
+        self.target_chat_ref: ExternalChat | None = None
