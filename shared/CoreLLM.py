73a073d4bc0ed3eb4a66a79f3a34b9204a775a70
LatekVon <latkaignacy@gmail.com>
2025-06-05 02:40:34 +0200

    use GroqCloud as default provider

--- shared/CoreLLM.py
+++ shared/CoreLLM.py
@@ -1,11 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from langchain_core.language_models import BaseChatModel
-from langchain_ollama import ChatOllama
+from langchain_groq import ChatGroq
 
-
-# todo: switch to GroqCloud, use Llama 4 Maverick (17Bx128E)
+load_dotenv()
 
 OLLAMA_MODEL = "llama3-groq-tool-use:8b"
-# GROQ_MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"
+GROQ_MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 
 class CoreLLM:
@@ -14,5 +17,6 @@ class CoreLLM:
     def __new__(cls, *args, **kwargs) -> BaseChatModel:
         if cls._llm:
             return cls._llm
-        cls._llm = ChatOllama(model=OLLAMA_MODEL)
+        # cls._llm = ChatOllama(model=OLLAMA_MODEL)
+        cls._llm = ChatGroq(model=GROQ_MODEL, api_key=GROQ_API_KEY)
         return cls._llm
